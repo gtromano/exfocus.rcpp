978a4e5e743f64f24dd5a7e483f26025a307d49c
gtromano <g.romano@lancaster.ac.uk>
2023-01-30 18:06:56 +0000

    npfocus function

---
+++ R/NPFOCuS.R
@@ -0,0 +1,25 @@
+npfocus_offline <- function(Y, threshold, quantiles, theta0 = rep(NaN, length(quantiles))) {
+
+  cppres <- .npfocus_offline(Y, quantiles, theta0, list(), adp_max_check = F)
+
+  sum_stat <- apply(cppres, 2, sum)
+  max_stat <- apply(cppres, 2, max)
+
+  n_sum <- which(sum_stat > threshold[1])[1]
+  n_max <- which(max_stat > threshold[2])[1]
+
+  n <- min(n_sum, n_max, na.rm = T)
+  return(list(sum_stat = sum_stat,
+              max_stat = max_stat,
+              n = n))
+}
+
+
+computeprobsNPPELT <- function (K, n = 100) {
+  probs <- vector(length = K)
+  c <- -log(2 * n - 1)
+  for(i in 1:K) {
+    probs[i] <- 1 / (1 +  (2 * n + 1) * exp( (c / K) * (2 * i - 1)) );
+  }
+  probs
+}
