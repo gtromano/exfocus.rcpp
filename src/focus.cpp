3b1bc4b85affb1212231fdbe4ed535c65fd670d7
gtromano <g.romano@lancaster.ac.uk>
2025-07-04 11:23:11 +0100

    cleaned calls to `update` function to remove various parameters that now are in the class constructor

--- src/focus.cpp
+++ src/focus.cpp
@@ -118,7 +118,6 @@ int get_tau_max (const Cost& Q, const CUSUM& cs, const double& theta0, const dou
 }
 
 
-
 // to write the adaptive maxima checking
 
 
@@ -126,71 +125,37 @@ int get_tau_max (const Cost& Q, const CUSUM& cs, const double& theta0, const dou
  * focus recursion, one iteration
  */
 
-void Info::update(const double& y, std::function<std::unique_ptr<Piece>(double, int, double)> newP, const double& thres, const double& theta0, const bool& adp_max_check) {
-
-  // updating the cusums and count with the new point
-  cs.n ++;
+void Info::update(const double& y) {
+  cs.n++;
   cs.Sn += y;
 
-  // std::cout << "iteration: " << cs.n << std::endl;
-  // std::cout << "cusum: " << cs.Sn << std::endl;
-
-
-  // updating the value of the max of the null (for pre-change mean unkown)
   auto m0val = 0.0;
   if (std::isnan(theta0))
     m0val = get_max(Qr.ps.front(), cs, theta0);
 
-  // std::cout << "m0val: " << m0val << std::endl;
+  prune(Qr, cs, theta0, true);
+  prune(Ql, cs, theta0, false);
 
-  // std::cout << "pruning step. size before pruning: " << Qr.ps.size() << std::endl;
-
-  // pruning step
-  prune(Qr, cs, theta0, true); // true for the right pruning
-  prune(Ql, cs, theta0, false); // false for the left pruning
-
-  // std::cout << "pruning step done. size after pruning: " << Qr.ps.size() << std::endl;
-
-
-  // check the maximum
-  if (adp_max_check) {
-    // std::cout << "to write " << std::endl;
-  } else {
-    // std::cout << "Qr maxs" << std::endl;
-
-    Qr.opt = get_max_all(Qr, cs, theta0, m0val);
-    // std::cout << "Ql maxs" << std::endl;
-
-    Ql.opt = get_max_all(Ql, cs, theta0, m0val);
-  }
-
-  // std::cout << "Qr opt: " << Qr.opt << " Ql opt: " << Ql.opt << std::endl;
-
-  // add a new point
+  Qr.opt = get_max_all(Qr, cs, theta0, m0val);
+  Ql.opt = get_max_all(Ql, cs, theta0, m0val);
 
   if (Qr.k < (Qr.ps.size() - 1)) {
-    Qr.k ++; // incrase the counter of the last piece position
+    Qr.k++;
     Qr.ps[Qr.k]->St = cs.Sn;
     Qr.ps[Qr.k]->tau = cs.n;
     Qr.ps[Qr.k]->m0 = m0val;
-  } else  {
+  } else {
     Qr.ps.push_back(std::move(newP(cs.Sn, cs.n, m0val)));
-    Qr.k = Qr.ps.size() - 1; // incrase the counter of the last piece position
+    Qr.k = Qr.ps.size() - 1;
   }
 
   if (Ql.k < (Ql.ps.size() - 1)) {
-    Ql.k ++; // incrase the counter of the last piece position
+    Ql.k++;
     Ql.ps[Ql.k]->St = cs.Sn;
     Ql.ps[Ql.k]->tau = cs.n;
     Ql.ps[Ql.k]->m0 = m0val;
-  } else  {
+  } else {
     Ql.ps.push_back(std::move(newP(cs.Sn, cs.n, m0val)));
-    Ql.k = Ql.ps.size() - 1; // incrase the counter of the last piece position
-
+    Ql.k = Ql.ps.size() - 1;
   }
-
-
-
-  // std::cout << "__________________________" << std::endl;
-
 }
